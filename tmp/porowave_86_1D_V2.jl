3c2d35d6260a575c96b00bdc50ad796b37f2e864
MathisBergogne <bergogne.mat@gmail.com>
2023-09-14 10:02:57 +0200

    update with speudo time and temperature

--- tmp/porowave_86_1D_V2.jl
+++ tmp/porowave_86_1D_V2.jl
@@ -3,7 +3,7 @@ default(xmirror = true,size=(1200, 800), framestyle=:box, label=false, grid=fals
 
 @views av(A) = 0.5 .* (A[1:end-1] .+ A[2:end])
 
-@views function porowave_1D_fluid(do_visu)
+@views function porowave_1D_86(do_visu)
 lx   = 50        # longeur du modèle
 kμ0  = 1         # permeabilite initial m2, viscosité fluide Pa s
 ϕ0   = 0.01      # porosité du fond
@@ -12,44 +12,35 @@ npow = 3
 ρfg  = 1         # masse volumique eau kg m-3, gravite terrestre m s-2
 ρsg  = 2         # masse volumique rocks kg m-3, gravite terrestre m s-2
 Δρg  = ρsg - ρfg # différence des masses volumiqueskg m-3, gravite terrestre m s-2
-βϕ   = 0.1       # compressibilité du fluide 
+Bϕ   = 0.1       # compressibilité du fluide 
 η    = 1         # matrix bulk viscosity
 # numerics
 nx   = 200       # nombre de noeuds de modèles en x
 dx   = lx / nx   # pas d'espace
 xc   = LinRange((1:nx) .* dx)   # coordonnées en x des noeuds du modèles
-nt   = 1e6       # nombre de pas de temps
+nt   = 1e7       # nombre de pas de temps
 dt   = 1e-3        # pas de temps
-nvis =  nt / 100
+nvis = nt / 100  #1e4
 # initialisation
 ϕ      = @. Δϕ  * exp(-(xc - (lx*3/4 + dx / 2))^2) + ϕ0
 Pe     = zeros(nx)
 qDx    = zeros(nx - 1)
 dPedt  = zeros(nx - 2)
 dϕdt   = zeros(nx - 2)
-# visualisation
-p1 = plot(ϕ, xc, title="ϕ",yaxis= :flip, xlims=(0,0.4));
-p2 = plot(Pe, xc, title="Pe",yaxis= :flip,xlims=(-0.15,0.15));
-display(plot(p1,p2))
 #loop
 for it = 1:nt 
     qDx   .= kμ0 .* av(ϕ).^npow .* (.- diff(Pe) ./ dx .+ Δρg)
-    dϕdt  .= diff(qDx) ./ dx
-    dPedt .= .- dϕdt .- ϕ[2:end-1] .* Pe[2:end-1] ./ η
-    Pe[2:end-1] .+= dt ./ ϕ[2:end-1] ./ βϕ .* dPedt     
-    ϕ[2:end-1]  .+= dt .* dϕdt
+    dPedt .= .- diff(qDx) .+ ϕ[2:end-1] .* Pe[2:end-1] ./ η
+    ϕ[2:end-1]  .+= dt .* (1 .- ϕ[2:end-1]) .* diff(qDx)
+    Pe[2:end-1] .+= ϕ[2:end-1] * Bϕ .* dPedt .* dt
     # visualisation
     if (it % nvis) == 0 && do_visu
-        p1 = plot(ϕ, xc, title="ϕ",yaxis= :flip, xlims=(0,0.4));
-        p2 = plot(Pe, xc, title="Pe",yaxis= :flip,xlims=(-0.15,0.15));
+        p1 = plot(ϕ, xc, title="ϕ",yaxis= :flip, xlims=(0,1.1*maximum(ϕ)))
+        p2 = plot(Pe, xc, title="Pe",yaxis= :flip) 
         display(plot(p1,p2))
     end
 end
 
-p1 = plot(ϕ, xc, title="ϕ",yaxis= :flip, xlims=(0,0.4));
-p2 = plot(Pe, xc, title="Pe",yaxis= :flip,xlims=(-0.15,0.15));
-display(plot(p1,p2))
-
 end
 
-porowave_1D_fluid(true)
+porowave_1D_86(true)
